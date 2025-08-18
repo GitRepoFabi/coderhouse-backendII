56d8812810f4e2ce04a48aff6cdad30eec70fce1
Fabian Larrosa <fabianlarrosa2013@gmail.com>
2025-08-18 09:31:39 -0300

    Borro comentarios de archivo products.model.js

--- src/DAO/mongo/models/products.model.js
+++ src/DAO/mongo/models/products.model.js
@@ -18,29 +18,3 @@ const productSchema = new mongoose.Schema({
 const productModel = mongoose.model("product", productSchema);
 
 export default productModel;
-
-
-
-
-
-// import mongoose from 'mongoose';
-
-// const productSchema = new mongoose.Schema({
-
-//     //Aquí procederemos a escribir todas las propiedades que queremos que tenga un producto en nuestra BD
-//     title: String,
-//     description: String,
-//     code: {
-//         type: Number,
-//         unique: true
-//     },
-//     price: Number,
-//     stock: Number,
-//     category: String,
-//     status: Boolean,
-//     thumbnails: Array
-// })
-
-// const productModel = mongoose.model("products",productSchema);
-
-// export default productModel;
